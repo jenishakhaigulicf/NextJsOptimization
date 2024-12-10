a3a85485eaa810e234efd5174854f24667d52443
Jenisha <jenisha@192.168.1.4>
2024-12-10 13:32:40 +0545

    10: configuring static page metadata

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
-import { Suspense } from 'react';
+import { Suspense } from "react";
 
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// must be exported
+// must be called metadata
+// must be in page.js or layout.js
+export const metadata = {
+  title: "LatestPost",
+  description: "Browse out latest post",
+};
 
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
@@ -14,9 +22,9 @@ export default async function Home() {
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
       <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
